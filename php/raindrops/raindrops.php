c5df0a9b6062754224a11e4681eddf056e4d55ed
Cedric Ziel <cedric@cedric-ziel.com>
2016-06-11 19:05:48 +0200

    Solve raindrops problem in php

---
+++ php/raindrops/raindrops.php
@@ -0,0 +1,18 @@
+<?php
+
+$primes = [1, 2, 3, 5, 7, 11, 13];
+
+function raindrops(int $number)
+{
+  $factors = [];
+
+  if (in_array(3, $factors)) {
+
+  } elseif (in_array(5, $factors)) {
+    # code...
+  } elseif (in_array(7, $factors)) {
+    # code...
+  } else {
+    return $number;
+  }
+}
