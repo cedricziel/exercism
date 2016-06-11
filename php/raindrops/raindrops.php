8dbf9ffc1e7a0f109d92228f69831735c130db3c
Cedric Ziel <cedric@cedric-ziel.com>
2016-06-11 19:12:28 +0200

    Add isogram problem for php

--- php/raindrops/raindrops.php
+++ php/raindrops/raindrops.php
@@ -1,18 +1,32 @@
 <?php
 
-$primes = [1, 2, 3, 5, 7, 11, 13];
-
-function raindrops(int $number)
+function raindrops(int $number) : string
 {
   $factors = [];
+  $primes = [1, 2, 3, 5, 7, 11, 13];
 
-  if (in_array(3, $factors)) {
-
-  } elseif (in_array(5, $factors)) {
-    # code...
-  } elseif (in_array(7, $factors)) {
-    # code...
-  } else {
-    return $number;
+  foreach (array_reverse($primes) as $prime) {
+    if (0 === ($number % $prime)) {
+      $factors[] = $prime;
+    }
   }
+
+  $output = '';
+  if (in_array(3, $factors)) {
+    $output .= "Pling";
+  }
+
+  if (in_array(5, $factors)) {
+    $output .= "Plang";
+  }
+
+  if (in_array(7, $factors)) {
+    $output .= "Plong";
+  }
+
+  if ('' === $output) {
+    $output .= (string)$number;
+  }
+
+  return $output;
 }
